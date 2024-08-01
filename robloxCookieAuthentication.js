98df9e5eb1741f1adb9bafb9c1202802f8d0a63c
Solomon Oduyebo <124704556+SolomonGM@users.noreply.github.com>
2024-08-01 16:24:54 +0100

    Update robloxCookieAuthentication.js

--- robloxCookieAuthentication.js
+++ robloxCookieAuthentication.js
@@ -1,21 +1,40 @@
-document.getElementById('useridForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+const ProfileUser = document.getElementById("User"); 
+const userInfoUrl = "https://users.roblox.com/v1/users/authenticated";
 
-    const profileID = document.getElementById('profileID').value;
-    const url = `https://users.roblox.com/v1/users/${profileID}`;
+const robloxCookieWarning = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_";
+const robloxCookieToken = "#"; //Replace with roblox cookie. 
+const robloxCookie = `.ROBLOSECURITY=${robloxCookieWarning}${robloxCookieToken}`
 
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-        return response.json();
-        })
-        .then(data => {
-            const userInfoDiv = document.getElementById('userInfo');
-            userInfoDiv.innerHTML = `<p><strong>Username:</strong> ${data.name}</p>`;
-        })
-        .catch(error => {
-            alert('Error fetching information')     
+async function getUserInfo(){
+    try {
+        const response = await fetch(userInfoUrl, {
+            method: 'GET',
+            headers: {
+                'Cookie': robloxCookie,
+                'Content-type': "application/json" 
+            },
+            credentials: 'include'    
         });
-});
+
+        if(!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+
+        const username = data.name; 
+        const id = data.id;
+        const displayName = data.displayName;
+
+        console.log(`Username: ${username}`)
+        console.log(`ID: ${id}`);
+        console.log(`Display Name: ${displayName}`);
+
+        ProfileUser.textContent = username;
+    } catch (error) {
+        console.error('Failed to fetch user information:', error);
+    }
+}
+
+
+getUserInfo();
